36fdb205fa6d5db7d5222646d126d22b7b94f9b5
Tuna Cevik <tunacevik0330@gmail.com>
2026-06-04 14:29:34 +0300

    chore: import week 11 django project as baseline

---
+++ publish/bump_version.py
@@ -0,0 +1,14 @@
+import sys
+
+def bump_version():
+    if len(sys.argv) > 1:
+        new_version = sys.argv[1]
+        with open("VERSION", "w") as f:
+            f.write(new_version)
+        print(f"Version updated to {new_version}")
+    else:
+        print("Error: No version string provided.")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    bump_version()
---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    ['@semantic-release/exec', {
+      prepareCmd: 'python3 publish/bump_version.py ${nextRelease.version}'
+    }],
+    ['@semantic-release/git', {
+      assets: ['CHANGELOG.md', 'VERSION'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    }],
+    '@semantic-release/github'
+  ]
+};
